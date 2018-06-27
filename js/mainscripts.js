be2bdda8069c631bde90808a312196086ac15e83
Jakub Wlodek <jwlodek@cs.stonybrook.edu>
2018-06-27 00:13:17 -0400

    added new pdf logo

--- js/mainscripts.js
+++ js/mainscripts.js
@@ -5,9 +5,7 @@ var youtube_logo = "assets/youtube_social_icon_red.png";
 var youtube_logo_dark = "assets/youtube_social_icon_dark.png";
 var github_mark = "assets/Github-Mark-64px.png";
 var github_mark_light = "assets/Github-Mark-Light-64px.png";
-var pdf_gray = "assets/pdf_gray.png";
-var pdf_blue = "assets/pdf_blue.png";
 
-function github_logo_hover(element, asset){
+function image_hover(element, asset){
     element.setAttribute('src', asset);
 }
