2852d1f2d67e8522ecb61e1a19952fd19bd1b95b
Jakub Wlodek <jwlodek@cs.stonybrook.edu>
2018-06-26 23:41:40 -0400

    changed mainscript so it wasn't hardcoded, added some more logos

--- js/mainscripts.js
+++ js/mainscripts.js
@@ -1,7 +1,13 @@
-function github_logo_hover(element){
-    element.setAttribute('src', 'assets/Github_Logo.png');
-}
 
-function github_logo_unhover(element){
-    element.setAttribute('src', 'assets/Github_Logo_White.png');
+var github_logo = "assets/Github_Logo.png";
+var github_logo_white = "assets/Github_Logo_White.png";
+var youtube_logo = "assets/youtube_social_icon_red.png";
+var youtube_logo_dark = "assets/youtube_social_icon_dark.png";
+var github_mark = "assets/Github-Mark-64px.png";
+var github_mark_light = "assets/Github-Mark-Light-64px.png";
+var pdf_gray = "assets/pdf_gray.png";
+var pdf_blue = "assets/pdf_blue.png";
+
+function github_logo_hover(element, asset){
+    element.setAttribute('src', asset);
 }
