8f637d5d84b275a88afa480f53c761412235bf1b
Jakub Wlodek <jwlodek@cs.stonybrook.edu>
2018-06-26 23:17:06 -0400

    added folder for javascript files. Also wrote two functions that will switch between logos on hover

---
+++ js/mainscripts.js
@@ -0,0 +1,7 @@
+function github_logo_hover(element){
+    element.setAttribute('src', 'assets/Github_Logo.png');
+}
+
+function github_logo_unhover(element){
+    element.setAttribute('src', 'assets/Github_Logo_White.png');
+}
