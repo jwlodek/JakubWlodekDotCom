3acbcd45b33c257c64ad5eaab5402db077dcc1da
Jakub Wlodek <jwlodek@cs.stonybrook.edu>
2018-06-28 08:56:47 -0400

    added linkedin logo

--- js/mainscripts.js
+++ js/mainscripts.js
@@ -5,6 +5,8 @@ var youtube_logo = "assets/youtube_social_icon_red.png";
 var youtube_logo_dark = "assets/youtube_social_icon_dark.png";
 var github_mark = "assets/Github-Mark-64px.png";
 var github_mark_light = "assets/Github-Mark-Light-64px.png";
+var linkedin_logo = "assets/Logo-White-128px-TM.png";
+var linkedin_logo_black = "assets/Logo-Black-128px-TM.png";
 
 function image_hover(element, asset){
     element.setAttribute('src', asset);
