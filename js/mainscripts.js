5d0a450470ba698af8cb8ab10ed3b5ed7c639747
Jakub Wlodek <jwlodek@cs.stonybrook.edu>
2018-07-08 00:06:58 -0400

    fixed annoying bug where github logos wouldn't work

--- js/mainscripts.js
+++ js/mainscripts.js
@@ -1,10 +1,10 @@
 
-var github_logo = "assets/Github_Logo.png";
-var github_logo_white = "assets/Github_Logo_White.png";
+var github_logo = "assets/GitHub_Logo.png";
+var github_logo_white = "assets/GitHub_Logo_White.png";
 var youtube_logo = "assets/youtube_social_icon_red.png";
 var youtube_logo_dark = "assets/youtube_social_icon_dark.png";
-var github_mark = "assets/Github-Mark-64px.png";
-var github_mark_light = "assets/Github-Mark-Light-64px.png";
+var mark = "assets/GitHub-Mark-64px.png";
+var mark_light = "assets/GitHub-Mark-Light-64px.png";
 var linkedin_logo = "assets/Logo-White-128px-TM.png";
 var linkedin_logo_black = "assets/Logo-Black-128px-TM.png";
 
