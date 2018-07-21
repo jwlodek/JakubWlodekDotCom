ffe31d4ec363d41eddf58e104c46f04989d086d6
jwlodek <jakub.wlodek@stonybrook.edu>
2018-07-20 21:28:41 -0400

    added bootstrap support finally fixed footer

--- js/mainscripts.js
+++ js/mainscripts.js
@@ -1,12 +1,10 @@
 
-var github_logo = "assets/GitHub_Logo.png";
-var github_logo_white = "assets/GitHub_Logo_White.png";
 var youtube_logo = "assets/youtube_social_icon_red.png";
 var youtube_logo_dark = "assets/youtube_social_icon_dark.png";
 var mark = "assets/GitHub-Mark-64px.png";
 var mark_light = "assets/GitHub-Mark-Light-64px.png";
-var linkedin_logo = "assets/Logo-White-128px-TM.png";
-var linkedin_logo_black = "assets/Logo-Black-128px-TM.png";
+var linkedin_logo = "assets/In-White-66px-TM.png";
+var linkedin_logo_black = "assets/In-Black-66px-TM.png";
 
 function image_hover(element, asset){
     element.setAttribute('src', asset);
