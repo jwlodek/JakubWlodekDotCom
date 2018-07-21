616fc1f94e81cb077ea215284a72cb0d027d82d8
jwlodek <jakub.wlodek@stonybrook.edu>
2018-07-20 22:05:02 -0400

    added new project to projects page+necessary assets

--- js/mainscripts.js
+++ js/mainscripts.js
@@ -5,6 +5,8 @@ var mark = "assets/GitHub-Mark-64px.png";
 var mark_light = "assets/GitHub-Mark-Light-64px.png";
 var linkedin_logo = "assets/In-White-66px-TM.png";
 var linkedin_logo_black = "assets/In-Black-66px-TM.png";
+var external_link = "assets/external_link.png";
+var external_link_white = "assets/external_link_white.png";
 
 function image_hover(element, asset){
     element.setAttribute('src', asset);
